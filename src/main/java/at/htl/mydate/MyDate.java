bfafaa9867c44f7ba46260a2f7e3c972d0179acf
QuirinEcker <33062936+QuirinEcker@users.noreply.github.com>
2018-10-11 19:57:42 +0200

    Quirin Ecker finished

--- src/main/java/at/htl/mydate/MyDate.java
+++ src/main/java/at/htl/mydate/MyDate.java
@@ -7,6 +7,72 @@ package at.htl.mydate;
  */
 public class MyDate {
 
+    private int day;
+    private int month;
+    private int year;
+    private String monthString;
+
+    public MyDate(int year, int month, int day) {
+        initializeFields(year, month, day);
+    }
+
+    private void initializeFields(int year, int month, int day) {
+        if (isvalid(year, month, day)) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+    }
+
+    private boolean isvalid(int year, int month, int day) {
+        return 1 <= year && year <= 3000 &&
+                1 <= month && month <= 12 &&
+                1 <= day && day <= 31;
+    }
+
+    public MyDate(String date) {
+        String[] dateComponents = date.split("\\.");
+        int day = Integer.parseInt(dateComponents[0]);
+        int month = Integer.parseInt(dateComponents[1]);
+        int year = Integer.parseInt(dateComponents[2]);
+
+        initializeFields(year, month, day);
+
+    }
+
+    //region Getter und Setter
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    //endregion
+
+    public Weekday getWeekday() {
+        if (isvalid(year, month, day)) {
+            int calculatedYear = adjustYearToFormula();
+            int weekday = ((day + (int)(2.6 * ((month + 9) % 12 + 1) - 0.2)
+                    + calculatedYear % 100 + (int)(calculatedYear % 100 / 4) + (int)(calculatedYear / 400)
+                    - 2 * (int)(calculatedYear / 100) - 1) % 7 + 7) % 7 + 1;
+            return Weekday.values()[weekday-1];
+        } else {
+            return null;
+        }
+    }
+
+    private int adjustYearToFormula() {
+        int calculatedYear = year;
+        if (month < 3) {
+           calculatedYear--;
+        }
+        return calculatedYear;
+    }
 
     /**
      * Formatierung des Datums
@@ -15,8 +81,7 @@ public class MyDate {
      */
     public String formatDate() {
 
-        return null;
-
+        return getWeekday().toString() + ", " + this.getDay() + ". " + getMonthString() + " " + this.getYear();
     }
 
     /**
@@ -27,8 +92,58 @@ public class MyDate {
      *         false, wenn this-Datum jünger oder gleich other-Datum ist
      */
     public boolean isYoungerThan(MyDate other) {
+        boolean isYounger = false;
 
-        return false;
+        isYounger = checkYear(other, isYounger);
 
+        return isYounger;
+    }
+
+    private boolean checkYear(MyDate other, boolean isYounger) {
+        if (this.getYear() > other.getYear()) {
+            isYounger = true;
+        } else if (this.getYear() == other.getYear()) {
+            isYounger = checkMonth(other, isYounger);
+        } else {
+            isYounger = false;
+        }
+        return isYounger;
+    }
+
+    private boolean checkMonth(MyDate other, boolean isYounger) {
+        if (this.getMonth() > other.getMonth()) {
+            isYounger = true;
+        } else if (this.getMonth() == other.getMonth()) {
+            isYounger = checkDay(other, isYounger);
+        } else {
+            isYounger = false;
+        }
+        return isYounger;
+    }
+
+    private boolean checkDay(MyDate other, boolean isYounger) {
+        if (this.getDay() > other.getDay()) {
+            isYounger = true;
+        } else {
+            isYounger = false;
+        }
+
+        return isYounger;
+    }
+
+    public String getMonthString() {
+        if (this.getMonth() == 1) return "Jänner";
+        else if (this.getMonth() ==  2) return "February";
+        else if (this.getMonth() ==  3) return "März";
+        else if (this.getMonth() ==  4) return "April";
+        else if (this.getMonth() ==  5) return "Mai";
+        else if (this.getMonth() ==  6) return "Juni";
+        else if (this.getMonth() ==  7) return "Juli";
+        else if (this.getMonth() ==  8) return "August";
+        else if (this.getMonth() ==  9) return "September";
+        else if (this.getMonth() == 10) return "Oktober";
+        else if (this.getMonth() == 11) return "November";
+        else if (this.getMonth() == 12) return "Dezember";
+        return null;
     }
 }
--- src/main/java/at/htl/mydate/Weekday.java
+++ src/main/java/at/htl/mydate/Weekday.java
@@ -1,11 +1,22 @@
 package at.htl.mydate;
 
 public enum Weekday {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY("Montag"),
+    TUESDAY("Dienstag"),
+    WEDNESDAY("Mittwoch"),
+    THURSDAY("Donnerstag"),
+    FRIDAY("Freitag"),
+    SATURDAY("Samstag"),
+    SUNDAY("Sonntag");
+
+    private final String germanName;
+
+    Weekday(String germanName) {
+        this.germanName = germanName;
+    }
+
+    @Override // taken from http://tutorials.jenkov.com/java/enums.html
+    public String toString() {
+        return germanName;
+    }
 }
